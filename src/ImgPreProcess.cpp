a5c2446f78a8ee6813f6e01c38aa39d3f24a6dc5
for-all-firstance <2123816717@qq.com>
2022-05-10 07:16:08 +0800

    Demo1

--- src/ImgPreProcess.cpp
+++ src/ImgPreProcess.cpp
@@ -1,46 +1,64 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
-#include<opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+#include "/home/pi/Desktop/xbotcon/include/DEBUG.h"
 using namespace cv;
 using namespace std;
 
-const Mat cameraMatrix = (Mat_<double>(3, 3) <<  1.2853517927598091e+03, 0., 3.1944768628958542e+02, 0.,
-       1.2792339468697937e+03, 2.3929354061292258e+02, 0., 0., 1.);//外参
-const Mat distCoeff = (Mat_<double>(1, 5) <<  -6.3687295852461456e-01, -1.9748008790347320e+00,
-       3.0970703651800782e-02, 2.1944646842516919e-03, 0. );//内参：畸变矩阵
-int gama = 10;
+int ImgPreProcess_DEBUG(Mat &img,Mat &dst)
+{
+       vector<Mat> channels;
+       split(img,channels);//分离通道
+       threshold(channels[0],dst, 100, 255, THRESH_BINARY);   
+       //threshold(channels[2],dst,0,255,THRESH_OTSU);
+       return 0;
+}
+
+int ImgPreProcess_ARMER(Mat &img,Mat &dst)
+{
+       vector<Mat> channels;
+       split(img,channels);//分离通道
+       Mat blueImg=channels.at(0)-channels.at(2);//装甲板颜色为蓝色，用蓝色减去红色可以获得最好的效果
+       threshold(blueImg,dst, 100, 255, THRESH_BINARY);
+       //threshold(blueImg,dst,0,255,THRESH_OTSU);
+       return 0;
+}
+
+int ImgPreProcess_ENERGY(Mat &img,Mat &dst)
+{
+       vector<Mat> channels;
+       split(img,channels);//分离通道
+       Mat greenImg=channels.at(1)-channels.at(2);//能量块为绿色
+       threshold(greenImg,dst, 100, 255, THRESH_BINARY)
+       //threshold(greenImg,dst,0,255,THRESH_OTSU);
+       return 0;
+}
+#if 0
+#include <ctime>
+int gama = 50;
 // static void on_ContrastandBright(int,void *);
 // int contrastvalue=80;//对比度和亮度都初始化为80
 // int brightness=80;
 
-int ImgPreProcess(Mat &img,Mat &dst,Size &imageSize,int mode)
+int ImgPreProcess(Mat &img,Mat &dst,Size &imageSize)
 {
-/////////////////调曝光和对比度(伽马增益)//////////
-       Mat gamaimg;
-       createTrackbar("gama","gama",&gama,300);//创建滑动条
-    // 构建查找表
-	Mat lookUpTable(1, 256, CV_8U);
-	uchar* p = lookUpTable.ptr();
-	for (int i = 0; i < 256; ++i)
-		p[i] = saturate_cast<uchar>(pow(i / 255.0, double(gama)/10) * 255.0);
-
-   // 使用查找表进行对比度亮度调整
-	double t3 = (double)getTickCount();
-	LUT(img, lookUpTable, gamaimg);
-	double time3 = ((double)getTickCount() - t3) / getTickFrequency();
-       imshow("gama",gamaimg);
-/////////////////////////////////////////////////////
-       Mat map1, map2,calibratedImg;
-       initUndistortRectifyMap(cameraMatrix, distCoeff, noArray(), cameraMatrix, imageSize, CV_32FC1, map1, map2);//对图像进行消歧
-       remap(gamaimg, calibratedImg, map1, map2, INTER_NEAREST);//对每一帧图像进行消歧
+       //clock_t startTime1 = clock();
+       //Mat calibratedImg;
+       //remap(img, calibratedImg, map1, map2, INTER_NEAREST);//对每一帧图像进行消歧
+       //clock_t endTime1 = clock();
+       //cout << "消歧用时："  << double(endTime1 - startTime1) / CLOCKS_PER_SEC << "s" << endl;
        vector<Mat> channels;
-       split(calibratedImg,channels);
-       Mat Blur,threashould;
-       GaussianBlur(channels[2],Blur,Size(7,7),0,0);//高斯滤波降噪
-       threshold(Blur,dst,0,255,THRESH_OTSU);
+       split(img,channels);//分离通道
+       //Mat Blur,threashould;
+       //GaussianBlur(channels[2],Blur,Size(7,7),0,0);//高斯滤波降噪
+       clock_t startTime2 = clock();
+       threshold(channels[2],dst,0,255,THRESH_OTSU);
+       clock_t endTime2 = clock();
+       cout << "OTSU用时："  << double(endTime2 - startTime2) / CLOCKS_PER_SEC << "s" << endl;
        imshow("preproccesed",dst);
        return 0;
 }
+#endif
 
 
--- src/position_adjust.cpp
+++ src/position_adjust.cpp
@@ -2,21 +2,49 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include </home/pi/Desktop/xbotcon/include/head.h>
 using namespace std;
 using namespace cv;
 
-int position_adjust(Point2f &target)
+const float width_error=20;
+const float height_error=20;
+int position_adjust(Point2f &target)//已弃用
 {
+    char message[3];
     if(target.x>=360)
         if(target.y>=240)
-            cout<<"装甲板在右下方"<<endl;
+            {
+                message[0]='r';
+                message[1]='d';//cout<<"装甲板在右下方"<<endl;
+                message[2]='\0';
+            }
         else
-            cout<<"装甲板在右上方"<<endl;
+            {
+                message[0]='r';
+                message[1]='u';//cout<<"装甲板在右上方"<<endl;
+                message[2]='\0';
+            }
     else
         if(target.y>=240)
-            cout<<"装甲板在左下方"<<endl;
+            {
+                message[0]='l';
+                message[1]='d';//cout<<"装甲板在左下方"<<endl;
+                message[2]='\0';
+            }
         else
-            cout<<"装甲板在左上方"<<endl;
+            {
+                message[0]='l';
+                message[1]='u';//cout<<"装甲板在左上方"<<endl;
+                message[2]='\0';
+            }
+            //communicate(message);
+    return 0;
+}
+int focused(Point2f target)
+{ 
+    Rect ceter_error(360,240,width_error,height_error);
+    if(target.inside(ceter_error))
+        return 1;
     return 0;
 }
 
