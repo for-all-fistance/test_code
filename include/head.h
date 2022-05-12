989bb788d6348a28b33fa43f5eb19f6638683da4
for-all-firstance <2123816717@qq.com>
2022-05-13 07:54:36 +0800

    Demo2

--- include/head.h
+++ include/head.h
@@ -1,17 +1,34 @@
 using namespace cv;
 using namespace std;
-float distancedetection_armer(vector<Point2f> refer_imgPoint);
-float distancedetection_energy(vector<Point2f> refer_imgPoint);
+struct armer//装甲板结构体
+{
+	Point2f armer_center;
+	vector<Point2f> armer_coners;
+	float height;
+	float width;
+	vector<RotatedRect> armer_light;
+	Rect2f armer_rect;
+	vector<Rect2f> armer_real_position;
+	vector<Point2f> armer_refer_imgPoint;//对应的装甲板2D图像点
+	Point2f point_pre;
+	Point2f point_angle;
+	Point2f real_point;
+	
+};
+//Point2f distancedetection_armer(vector<Point2f> refer_imgPoint);
+float distancedetection_energy(vector<armer> armers);
 int color_picker(Mat img);
-void getTarget2dPosition(const cv::RotatedRect& rect, std::vector<std::vector<Point2f>> &target2d, const cv::Point2f& offset) ;
+void getTarget2dPosition(vector<armer>& armers, const cv::Point2f& offset);
 int ImgPreProcess_DEBUG(Mat &img,Mat &dst);
 int ImgPreProcess_ARMER(Mat &img,Mat &dst);
 int ImgPreProcess_ENERGY(Mat &img,Mat &dst);
-void SetROI(Mat& img,const Point2f& armer_center);
-Point2f kalman_filter(vector<Point2f> rect);
-int armerClassifier(Mat &img,Mat origin,vector<RotatedRect> &armer_rect,vector<Point2f> &armer_center);
-int energyClassifier(Mat &img,Mat origin,vector<RotatedRect> &energy_rect);
+void SetROI(Mat& img,const vector<armer> armers);
+void kalman_filter(vector<armer> &armers);
+int armerClassifier(Mat &img,vector<armer>&);
+int energyClassifier(Mat &img,Mat origin,vector<armer> armers);
 int position_adjust(Point2f &);
 int focused(Point2f target);
 int communicate(char const *message,int const fd);
+void gravity_offset_composite(vector<armer> &armers);
+
 
--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <vector>
 #include <stdio.h>
 #include <cmath>
+#include "platform/inc/vl53l0x_types.h"
 #if 1
 #include <ctime>//测试用
 #include <iostream>
@@ -21,21 +22,19 @@ using namespace cv;
 using namespace std;
 
 
+//VideoCapture cap("/home/pi/Desktop/xbotcon/armer test.avi");
 VideoCapture cap(0);
 const int imageWidth = 640; //定义图片大小，即摄像头的分辨率
 const int imageHeight = 480;
-vector<RotatedRect> armer_rect;//储存识别到的装甲板灯条位置
-vector<RotatedRect> energy_rect;//储存能量块位置
-vector<Point2f> armer_center;//匹配后的装甲板中心位置
 bool success =false;
-const Mat cameraMatrix = (Mat_<double>(3, 3) <<  987.8162802728854, 0, 515.7196277867043,
- 0, 999.9341934371877, 383.1530462255868,
+const Mat cameraMatrix = (Mat_<double>(3, 3) <<  611.6348658352435, 0, 317.0063484473284,
+0, 618.2053424684727, 238.8843698515634,
  0, 0, 1);//外参
-const Mat distCoeff = (Mat_<double>(1, 5) <<  0.2181693038172464, -0.5750471376539981, -0.00785331353651738, 0.0046813462975, 0.3820630120717159 );//内参：畸变矩阵
+const Mat distCoeff = (Mat_<double>(1, 5) << 0.1252247200301222, 0.3344879529935187, 0.004913024896752389, -0.004298159448975511, -2.743281072337644 );//内参：畸变矩阵
 mutex mutex_fd;
 mutex mutex_send;
-std::condition_variable cv_send;
-void get_thread(char& command,int fd)//获取指令的线程
+condition_variable cv_send;
+void get_thread(char& command,int fd)//接受指令的线程
 {
 	while(true)
 	{
@@ -44,31 +43,32 @@ void get_thread(char& command,int fd)//获取指令的线程
 		{
 			command=serialGetchar(fd);
 			cout<<"get command"<<command<<endl;
-			serialFlush(fd); //avoid  repeat reception
+			serialFlush(fd); //清空缓冲区，防止重复接收
 		}
 		lock_fd.unlock();
-		this_thread::sleep_for(std::chrono::milliseconds(50));//等待五十毫秒
+		this_thread::sleep_for(std::chrono::milliseconds(50));//休眠50ms
 	}
 }
-void send_thread(int fd,Point2d& point_angle)//向下位机发送数据线程
+void send_thread(int fd,Point2f &point_angle)//发送数据给下位机的线程
 {
 	char *message;
 	while(true)
 	{
 		unique_lock <mutex> lock(mutex_send);
 		cv_send.wait(lock);
+		unique_lock<mutex> lock_fd(mutex_fd);
 		message=new char[5];
-		snprintf(message,5,"%d",int(point_angle.x*1000));
+		snprintf(message,5,"%d",int(point_angle.x*10000));
 		communicate("x",fd);
 		communicate(message,fd);
 		communicate("y",fd);
-		snprintf(message,5,"%d",int(point_angle.y*1000));
+		snprintf(message,5,"%d",int(point_angle.y*10000));
 		communicate(message,fd);
 		delete message;
 	}
 }
 
-Point2d angle_solver(Point2f &P_oc)//解算角度
+Point2d angle_solver(Point2f &P_oc)
 {
 	static double cx=cameraMatrix.at<double>(0,2);
 	static double cy=cameraMatrix.at<double>(1,2);
@@ -76,23 +76,21 @@ Point2d angle_solver(Point2f &P_oc)//解算角度
 	static double fy=cameraMatrix.at<double>(1,1);
 	double rx=(P_oc.x-cx)/fx;
     double ry=(P_oc.y-cy)/fy;
-    return(Point2d(atan(rx)/CV_PI*180,atan(ry)/CV_PI*180));
+    return(Point2f(atan(rx)/CV_PI*180,atan(ry)/CV_PI*180));
 }
 ///////////////////////////////////////////////////////////////////
 int main() 
 {
 	cap.set(CAP_PROP_FOURCC, VideoWriter::fourcc('M', 'J', 'P', 'G'));
-	//cap.set(CAP_PROP_FRAME_HEIGHT,960);
-	//cap.set(CAP_PROP_FRAME_WIDTH,720);
-	/////////test//////////////
 	Mat img;
 	Size imageSize = Size(imageWidth, imageHeight);
-	vector< vector<Point2f>> armer_refer_imgPoint;//对应的装甲板2D图像点
+	vector<RotatedRect> energy_rect;
 	vector< vector<Point2f>> energy_refer_imgPoint;//对应的能量条2D图像点
-	vector<Point2d> point_pre;
-	Point2d point_angle;
-	char command='~';
-	char *message;
+	Point2f point_angle;
+	vector<armer> armers;
+	armer init_armer {Point2f(320,240),{},0,0,{},Rect(),{},{},Point2f(0,0),Point2f(0,0),Point2f(0,0)};
+	armers.push_back(init_armer);
+	char command='%';
 	//cout<<wiringPiSetup()<<endl;
 	if(-1==wiringPiSetup())
 		{
@@ -109,48 +107,50 @@ int main()
 	thread send_th=thread(send_thread,fd,std::ref(point_angle));
 	while (true)
 	{
-		clock_t startTime = clock();//test
+		
 		cap >> img;
+		clock_t startTime = clock();//test
 		if(img.empty())
 			{
 				cout<<"no img ,error"<<endl;
 				break;
 			}
-		if(command=='~')//等待开始射击
+		if(command=='~')
 		{
 			cout<<"stand by"<<endl;
 			waitKey(40);//wait 40ms for the next command
 		}
-		else if(command=='%')//开始射击装甲板
+		else if(command=='%')
 		{
 			Mat dst;
 			clock_t startTime1 = clock();
 			if(success)
 				{
-					//SetROI(img,armer_center.back());
+					//SetROI(img,armer_real_position.back());
 					//imshow("ROI",img);
 				}
 			ImgPreProcess_ARMER(img,dst);
 			clock_t endTime1 = clock();
 			cout << "preprocess用时："  << double(endTime1 - startTime1) / CLOCKS_PER_SEC << "s" << endl;
-			success=armerClassifier(dst,img,armer_rect,armer_center);
+			success=armerClassifier(dst,armers);
+			clock_t endTime2 = clock();
+			cout << "classifier用时："  << double(endTime2 - endTime1) / CLOCKS_PER_SEC << "s" << endl;
 			if(success)
 			{
-				getTarget2dPosition(armer_rect.back(),armer_refer_imgPoint,Point2f(0,0));//将armer_rect 中的点按顺序排列
-				//Point2f temp=armer_center.back();
-				//temp.y+=distancedetection_armer(armer_refer_imgPoint.back());//线性修正
-				//point_pre.push_back(angle_solver(kalman_filter(armer_center)));
-				distancedetection_armer(armer_refer_imgPoint.back());//测距，暂时无用
-				vector<Point2f> target_center={Point2f(armer_center.back().x+320,armer_center.back().y+240)};
-				undistortPoints(target_center,target_center,cameraMatrix,distCoeff,noArray(),cameraMatrix);//消畸
-				cout<<armer_center.back()<<endl;
+				getTarget2dPosition(armers,Point2f(0,0));
+				for(int i=0;i<4;++i)
+					circle(img,armers.back().armer_refer_imgPoint.at(i),5,Scalar(120,200,0),FILLED);
+				undistortPoints(armers.back().armer_refer_imgPoint,armers.back().armer_refer_imgPoint,cameraMatrix,distCoeff,noArray(),cameraMatrix);
+				armers.back().armer_center=(armers.back().armer_refer_imgPoint[1]+armers.back().armer_refer_imgPoint[3])/2.0;
+				gravity_offset_composite(armers);
+				kalman_filter(armers);
 				unique_lock <mutex> lock(mutex_send);
-				point_angle=angle_solver(target_center.at(0));
-				cout<<point_angle<<endl;
-				cv_send.notify_one();//唤醒被阻塞的send_th线程
-				
+				point_angle=angle_solver(armers.back().armer_center);
+				cout<<"point_angle:"<<point_angle<<endl;
+				cv_send.notify_one();
 			}
 		}
+		#if 0
 		else if(command=='&')
 		{
 			Mat dst;
@@ -166,8 +166,9 @@ int main()
 				cv_send.notify_one();
 			}
 		}
-		
-		waitKey(100);
+		#endif
+		//imshow("img",img);
+		//waitKey(1);
 		clock_t endTime = clock();
 		cout << "该帧用时：" << double(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
 	}
@@ -177,9 +178,9 @@ int main()
 	return 0;
 }
 
-void SetROI(Mat& img,const Point2f& armer_center)
+void SetROI(Mat& img,const Point2f& armer_real_position)
 {
-		float x=armer_center.x,y=armer_center.y;
+		float x=armer_real_position.x,y=armer_real_position.y;
 		float collom_begin=(x>200)?200:x,row_begin=(y>150)?150:y,collom_end=(330>y)?150:(480-y),row_end=(440>x)?200:(640-x);
 		img=img(Range(y-row_begin,y+row_end),Range(x-collom_begin,x+collom_end));
 	//imshow("roi img",img);
